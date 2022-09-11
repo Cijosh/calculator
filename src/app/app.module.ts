15730a0c81422d2de8a9a86adbc6ab4334bfc155
cijosh <cijoshkjohn@gmail.com>
2022-09-11 11:40:35 +1000

    project init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { KeyboardComponentComponent } from './components/keyboard-component/keyboard-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    KeyboardComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbPaginationModule, 
+    NgbAlertModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/keyboard-component/keyboard-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard-component',
+  templateUrl: './keyboard-component.component.html',
+  styleUrls: ['./keyboard-component.component.css']
+})
+export class KeyboardComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
